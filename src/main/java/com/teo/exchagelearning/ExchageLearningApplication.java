f32c57c0154dcbb4ca29626545d355e13856d8c8
Teodora Reu <teodora.reu@student.manchester.ac.uk>
2021-09-10 11:42:35 +0100

    Initial commit

---
+++ src/main/java/com/teo/exchagelearning/ExchageLearningApplication.java
@@ -0,0 +1,13 @@
+package com.teo.exchagelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchageLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchageLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teo/exchagelearning/ExchageLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teo.exchagelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchageLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
